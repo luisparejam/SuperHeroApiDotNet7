5099b457f757f5d95b8db5b2e98a028879f4ce39
Luis Pareja <lpareja@hotmail.com>
2023-09-20 07:33:59 -0400

    Initial commit

---
+++ Controllers/SuperHeroController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperHeroApiDotNet7.Models;
+using SuperHeroApiDotNet7.Services.SuperHeroService;
+
+namespace SuperHeroApiDotNet7.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private readonly ISuperHeroService _superHeroService;
+
+        public SuperHeroController(ISuperHeroService superHeroService)
+        {
+            _superHeroService = superHeroService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetAllHeroes()
+        {
+            var result = await _superHeroService.GetAllHeroes();
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult<SuperHero>> GetSingleHero(int id)
+        {
+            var result = await _superHeroService.GetSingleHero(id);
+            if (result == null)
+                return NotFound("Hero not found.");
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> AddHero(SuperHero hero)
+        {
+            var result = await _superHeroService.AddHero(hero);
+            return Ok(result);
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> UpdateHero(int id, SuperHero request)
+        {
+            var result = await _superHeroService.UpdateHero(id,request);
+            if (result == null)
+                return NotFound("Hero not found.");
+            return Ok(result);
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> DeleteHero(int id)
+        {
+            var result = await _superHeroService.DeleteHero(id);
+            if (result == null)
+                return NotFound("Hero not found.");
+            return Ok(result);
+        }
+    }
+}
